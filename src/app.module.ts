935b07ff526ebf8baad17191e0dd08250daa725f
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-06-07 15:08:58 +0200

    modified: app.module.ts for TypePrmModule.forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [CustomersModule, UsersModule, TypeOrmModule.forRoot(
+    {
+      type: 'postgres',
+      host: 'localhost',
+      port: 1234,
+      username: "nestjs",
+      password: "nestjs",
+      database: "nestjs",
+      entities:  [],
+      synchronize: true
+    }
+  )],
   controllers: [],
   providers: [],
 })
